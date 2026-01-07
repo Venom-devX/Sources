7bc37d10f4d669500dd598c257f3b94e93a6bb79
Venom DevZ <davilfluna@gmail.com>
2026-01-07 16:20:38 -0300

    dsawedssefddsrgfdthgyjhgyjsajehfdbsefdje
    fcvg thbyn jugbfvc bhtyhgfbv

---
+++ Modules/Bloxburg/BuildAPI.lua
@@ -0,0 +1,40 @@
+IKfCF
+VJ8;Y
+[_EaBY_YMQgVBRUVV_]!Wj\_PKD3[QVDTVcYTWQAJ9nX
+URT!XCdS]PFWDXQMa]GT
+W]U6[GRRH)V\GTqEQ7PO\RPK5h>oN0LPF9lU_PS_UTeWUWW"LW_\C[AUUWTFE@YEQK'UU1SAN\\(Y@Q0PJEVFQl>	\[VTCSjT@P!CR\DRJVU@OT]@&RR0WG@[\Ls]\cV@EQ@D52k8VZU
+YE[YCT@D7OP@OC`D>2El0DefLTl>EJCVn>P
+Q:lV[STFD[EY[E^PEgU
+GrUF4hF\\QRXPPAFF[UJU_	
+IH:oSBEHaIDC	DZZ>2ERBZCTJT@98P
+Ul>	\[VTCC[EZPC4TWF]NCR
+F#L[RAWh9C	WZY\D]AwYQLQ	\DXWDQV1
+R#GYSE[_
+	GSfY
+BV}CWCl=DE
+S]\@REA@
+VV:2CFBECDG
+[CNFY[TAWAEKP\Z]BU]F\Yo?F@TB@VG
+ZM
+XHMHHEY?=X8:9iCDSGMGPCSlJCQ
+B'APL^W
+O;h]_n>YVV
+D_M\[
+^[DRQYMXGVFN@#F
+QA_
+Vo?F_]PU^R_WLY^dVMEAXAvXAY[
+J9nBTZX[AYS$FFSWA6T[VyE_NW_BLn>DMM@VFTXEU^|OZZP1]EAK\PA$J^9l\^W?998Iz[]x4-;9
+	Au[[TpbyCFNM9in>AUGQZu^Z$d*zFZXVs_QM]YZ]OFEYC3[FHRiWAknD\VUPGKBEZ
+APDl@P4:Div_dDY]MhkE85AQF3[?FXEgYI9iDjCd
+@j^FS\a
+9?DFh
+RP+SZV<?BBh9C]MGZFZQ_^U@VLJWM43
+R>l^
+\rD[\w6|v
+Xp
+YA[TRME
+Q5TTFVACQ~WS]JWBLn>D
+ZY^VFD
+[\GEaH>iEBoDwQ^WiDYSU]]?nDDFC=C6SjC
+RcQXDOWWZdSS2XPeRXGPi;AEC=6YC@xQR
+FdCRE}QU<?BBECn5WJ@h[@\A98DEF4UXyQGH:o?=S@BZCUXJWA_FICBSMh>[\>998GEFzBQw2q
